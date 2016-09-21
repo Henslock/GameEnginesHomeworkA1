2e057a1ac14aeb330160b4f7427dc56871251bbe
Owen <o.r.meier@gmail.com>
2016-09-21 17:53:13 -0400

    Read Me updated. Script updated.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # GameEnginesHomeworkA1
 Squash and Stretch Demo
 
-We'll fill this read-me out later. I swear.
+100526009 - Josh Bellyk
+100538643 - Owen Meier
+
+Hello, and welcome to our squash and stretch assignment featuring the amazing squishy rubber ducky (NOT FOR SALE).
+
+	Navigate to PROJECTFOLDER > Assets > _Project > Scenes > SquashNStretch.Unity to open our scene.
+	
+In play mode, you can observe 3 glorious ducks. The one on the right is animated using Maya, and the other on the left
+is animated using scripts in Unity. The third mysterious duck in the background is for experimentation.
+There are two component scripts, the main script to pay attention to is "SnSAnim".
+This script can be applied to the handle of any model and it will demonstrate a jumping animation with squash
+and stretch. You can control parameters such as; speed, jump height, and SnS factor in the script component window.
+
+Speed - Affects the speed of the animation.
+Jump Height - Affects how high the model will jump in the animation.
+Squash Factor - Affects the distortion rate of squash and stretch.
+
+The animation was set-up using a basic key-frame system and LERP vector functions.
+
+Model created by Owen Meier and tweaked by Josh Bellyk (I gave the head a makeover).
+
+
+The end. So long and thanks for all the fish.
--- Squash And Stretch Demo/Assets/_Project/Scripts/SnSAnim.cs
+++ Squash And Stretch Demo/Assets/_Project/Scripts/SnSAnim.cs
@@ -10,15 +10,16 @@ public class SnSAnim : MonoBehaviour
     Owen Meier  - 100538643    
     */
 
-    public float t;
+    private float t;
     private int keyframe;
-    private float duration = 0.3f;
-    private bool flip = false;
+    public float Speed = 0.3f;
+    public float jumpHeight = 0.5f;
+    public float SquishFactor = 1.0f;
 
     Vector3 m1 = new Vector3(1.0f, 1.0f, 1.0f);
-    Vector3 m2 = new Vector3(1.1f, 0.9f, 1.1f);
-    Vector3 m3 = new Vector3(0.9f, 1.1f, 0.9f);
-    private float jumpHeight = 0.5f;
+    Vector3 m2;
+    Vector3 m3;
+
     private Vector3 p1;
     private Vector3 p2;
 
@@ -26,33 +27,37 @@ public class SnSAnim : MonoBehaviour
     {
         t = 0.0f;
         keyframe = 0;
+
         p1 = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
         p2 = new Vector3(transform.localPosition.x, transform.localPosition.y + jumpHeight, transform.localPosition.z);
+
+        m2 = new Vector3(1.0f * SquishFactor, 1.0f / SquishFactor, 1.0f *SquishFactor);
+        m3 = new Vector3(1.0f / SquishFactor, 1.0f * SquishFactor, 1.0f / SquishFactor);
     }
 
     void Update()
-    {
+    {   
         t += Time.deltaTime;
         if (keyframe == 0)
         {
-            transform.localScale = Vector3.Lerp(m1, m2, t / duration);
+            transform.localScale = Vector3.Lerp(m1, m2, t / Speed);
         }
         if (keyframe == 1)
         {
-            transform.localScale = Vector3.Lerp(m2, m1, t / duration);
+            transform.localScale = Vector3.Lerp(m2, m1, t / Speed);
         }
         if (keyframe == 2)
         {
-            transform.localPosition = Vector3.Lerp(p1, p2, t / duration);
-            transform.localScale = Vector3.Lerp(m1, m3, t / duration);
+            transform.localPosition = Vector3.Lerp(p1, p2, t / Speed);
+            transform.localScale = Vector3.Lerp(m1, m3, t / Speed);
         }
         if (keyframe == 3)
         {
-            transform.localPosition = Vector3.Lerp(p2, p1, t / duration);
-            transform.localScale = Vector3.Lerp(m3, m1, t / duration);
+            transform.localPosition = Vector3.Lerp(p2, p1, t / Speed);
+            transform.localScale = Vector3.Lerp(m3, m1, t / Speed);
         }
 
-        if (t >= duration)
+        if (t >= Speed)
         {
             keyframe++;
             t = 0.0f;
